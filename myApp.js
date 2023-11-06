1660bfa80c6c829812a272c66fb80133464df0c7
Davi Bogner <davibogner@hotmail.com>
2023-11-06 17:31:13 -0300

    teste

--- myApp.js
+++ myApp.js
@@ -1,38 +1,15 @@
-console.log(    "Hello World"    );
 
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  console.log("Hello World")
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
